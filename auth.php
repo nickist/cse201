39d6db58072b4e862c33b2fb986300de39f3b24e
Nick <nick@pop-os.localdomain>
2019-03-30 20:02:44 -0400

    removed config.php updated databaseCreate script

--- auth.php
+++ auth.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     if(empty($_SESSION['user'])){
-        header('location: /stateline/login.html');
+        header('location: /cse201/login.html');
         exit;
     }
 ?>
--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $DB_HOST = '127.0.0.1:3306';
-    $DB_USER = 'stateline';
-    $DB_PASS = 'JFK%sigma314';
-    $DB_NAME = 'statelinedb';
-?>

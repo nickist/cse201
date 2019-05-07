7ecc5459cdbea67cac5419f80328f062e4a177f8
nickist <njkist@gmail.com>
2019-05-07 09:31:33 -0400

    fixed bug in setup.sh

--- setup.sh
+++ setup.sh
@@ -19,6 +19,7 @@ GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.db WHERE Db='test' OR Db='test_%';
+DROP DATABASE IF EXISTS thatzthebookdb;
 CREATE DATABASE  thatzthebookdb;
 FLUSH PRIVILEGES;
 DROP USER IF EXISTS 'thatzthebookuser'@'localhost';
